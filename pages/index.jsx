f859898384ec39782761c7d503e07c5c2c037b98
Oscar Gomez <oscarthedev@gmail.com>
2022-09-19 21:42:53 -0600

    [ADD] "hero" to index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,16 @@
 import Head from "next/head"
+import React from "react"
+import Hero from "../components/Hero/hero"
 
 export default function Home() {
   return (
-    <div>
+    <React.Fragment>
 		<Head>
 			<title>Óscar Gómez's Portfolio</title>
 			<meta name="description" content="Óscar Gómez's Portfolio" />
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
-    </div>
+		<Hero/>
+    </React.Fragment>
   )
 }
