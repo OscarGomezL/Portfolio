96c1b5ade0c5618b0456cdd09d9787483cab28e3
Oscar Gomez <oscarthedev@gmail.com>
2022-09-19 21:02:05 -0600

    [ADD] "variables.css" file to styles dir & imported

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/reset.css'
 import '../styles/fonts.css'
 import '../styles/animations.css'
+import '../styles/css.css'
+import '../styles/variables.css'
+import '../styles/Hero/hero.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
