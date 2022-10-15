fe30035ce7c5f577a553432f2648b68406ac871e
Oscar Gomez <oscarthedev@gmail.com>
2022-10-15 13:08:39 -0600

    [ADD] only client side code with useEffect due to ssr

--- components/AboutMe/title.tsx
+++ components/AboutMe/title.tsx
@@ -1,4 +1,28 @@
+import { useState, useEffect } from "react";
+import { aboutMeTitleObserver } from '../../js/observer.js'
+
 export default function Title() {
+	useEffect(() => {
+		async function waitForClientSide() {
+			return new Promise(resolve => {
+				if(typeof window !== "undefined") {
+					resolve(true)
+				}
+				else {
+					waitForClientSide()
+				}
+			});
+		}
+		async function asyncCall() {
+			const result = await waitForClientSide();
+			if(result) {
+				const aboutMeTitle = document.querySelector('.aboutMe_title') as HTMLElement 
+				aboutMeTitleObserver().observe(aboutMeTitle)
+			}
+			else asyncCall()
+		}
+		asyncCall();
+	}, [])
 	return (
 		<div 
 			className="aboutMe_title"
