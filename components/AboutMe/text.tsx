453b398f5ee3c3f9c1b4e2d429c00fab7dea13a8
Oscar Gomez <oscarthedev@gmail.com>
2022-10-15 12:51:28 -0600

    [ADD] "aboutMe" section text component added

---
+++ components/AboutMe/text.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Text() {
+	return (
+		<div className='aboutMe_text_container'>
+			<div className='aboutMe_line_1 aboutMe_line'>An "About Me"</div>
+			<div className='aboutMe_line_2 aboutMe_line'>Section is here</div>
+			<div className='aboutMe_line_3 aboutMe_line'>A description Will</div>
+			<div className='aboutMe_line_4 aboutMe_line'>Be Right Here</div>
+		</div>
+	)
+}
