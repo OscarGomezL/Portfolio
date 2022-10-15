cf4378b4d53a27abb00799f7a96e3081b227b8b9
Oscar Gomez <oscarthedev@gmail.com>
2022-10-15 12:47:10 -0600

    [ADD] home page imports patched +  migrated from jsx to tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import React from "react"
 import Hero from "../components/Hero/hero"
-import AboutMe from "../components/About Me/aboutMe"
+import AboutMe from "../components/AboutMe/aboutMe"
 import Projects from "../components/Projects/projects"
 import Skills from "../components/Skills/skills"
 import Contact from "../components/Contact/contact"
