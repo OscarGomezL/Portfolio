aa4936a6245785ba29ada84c71c05a31f4c9bf11
Oscar Gomez <oscarthedev@gmail.com>
2022-10-15 12:48:41 -0600

    [ADD] "aboutMe" section

---
+++ components/AboutMe/aboutMe.tsx
@@ -0,0 +1,11 @@
+import Text from "./text"
+import Title from './title'
+
+export default function AboutMe() {
+	return (
+		<section className="container aboutMe">
+			<Text/>
+			<Title/>
+		</section>
+	)
+}
