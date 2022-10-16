bb5109e47d3a1681e50cc037ba1a82e4fd610389
Oscar Gomez <oscarthedev@gmail.com>
2022-10-16 11:28:03 -0600

    [ADD] components imported to projects section

--- components/Projects/projects.tsx
+++ components/Projects/projects.tsx
@@ -1,7 +1,21 @@
+import Number from "./number"
+import Info from "./info"
+import Controls from "./controls"
+import Title from "./title"
+import Images from "./images"
+
 export default function Projects() {
 	return (
 		<section className="container projects">
-
+			<div className="projects_container_1">
+				<Number/>
+				<Info/>
+				<Title/>
+				<Controls/>
+			</div>
+			<div className="projects_container_2">
+				<Images/>
+			</div>
 		</section>
 	)
 }
