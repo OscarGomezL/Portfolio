e21c11fcab36ca53783a30355b413d2878064a3a
Oscar Gomez <oscarthedev@gmail.com>
2022-10-21 22:16:00 -0600

    [ADD] svgs in use for "controls.tsx" component

--- components/Projects/controls.tsx
+++ components/Projects/controls.tsx
@@ -1,7 +1,8 @@
 export default function Controls() {
 	return (
 		<div className="projects_controls">
-			{"< >"}
+			<img src="/polygon_1.svg" alt="<"/>
+			<img src="/polygon_2.svg" alt=">"/>
 		</div>
 	)
 }
