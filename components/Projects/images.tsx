8e3002566c2a000dd1174a712a798b68dc65454d
Oscar Gomez <oscarthedev@gmail.com>
2022-10-21 22:17:32 -0600

    [ADD] "images.tsx" component structure completed

--- components/Projects/images.tsx
+++ components/Projects/images.tsx
@@ -1,6 +1,36 @@
+import Image from 'next/future/image'
+import ipad from '../../public/snickers/snickers_ipad.png'
+import ipad_2 from '../../public/snickers/snickers_ipad2.png'
+import phone from '../../public/snickers/snickers_phone.png'
+
 export default function Images() {
 	return (
 		<div className="projects_images">
+			<div className='projects_images_1'>
+					<Image
+						width={286}
+						height={440}
+						priority={true}
+						src={ipad}
+						className="img_projects_1"
+					/>
+					<Image
+						width={158}
+						height={321}
+						priority={true}
+						src={phone}
+						className="img_projects_2"
+					/>
+			</div>
+			<div className='projects_images_2'>
+					<Image
+						width={440}
+						height={286}
+						priority={true}
+						src={ipad_2}
+						className="img_projects_3"
+					/>
+			</div>
 		</div>
 	)
 }
