6b09e632bb34e2575a7996609d094585acd9fe1d
Oscar Gomez <oscarthedev@gmail.com>
2022-10-21 22:18:20 -0600

    [ADD] projects "info.tsx" component structure completed

--- components/Projects/info.tsx
+++ components/Projects/info.tsx
@@ -1,9 +1,15 @@
 export default function Info() {
 	return (
 		<div className="projects_info">
-			Snickers Arcade
-			Html Bootstrap js
-			Snickers Arcade Is A ...
+			<div className="projects_info_title">
+				Snickers Arcade
+			</div>
+			<div className="projects_info_technologies">
+				Html  ∙ Bootstrap ∙ JS
+			</div>
+			<div className="projects_info_description">
+				Snickers Arcade is a project in which I participated while working in Accenture. Snickers made a campaign in my country for which I worked along with my team developing the frontend of the software they used for the giveaway promotion. 
+			</div>
 		</div>
 	)
 }
