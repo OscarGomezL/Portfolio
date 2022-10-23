a69a640a96fa3aa51f343c5235f460c283b9e1fd
Oscar Gomez <oscarthedev@gmail.com>
2022-10-22 22:14:49 -0600

    [ADD] "contact.css" file added & imported

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,8 @@ import '../styles/fonts.css'
 import '../styles/animations.css'
 import '../styles/Hero/hero.css'
 import '../styles/About Me/aboutMe.css'
-import '../styles/projects/projects.css'
+import '../styles/Projects/projects.css'
+import '../styles/Contact/contact.css'
 import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }:AppProps) {
