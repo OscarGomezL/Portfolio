bfc34a34679a120fe265b68fec5954580fa58e1b
Oscar Gomez <oscarthedev@gmail.com>
2022-10-23 21:08:08 -0600

    [ADD] links added to "contact.tsx" component

--- components/Contact/contact.tsx
+++ components/Contact/contact.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/future/image'
+import Link from 'next/link'
 import git from '../../public/social/git.svg'
 import linkedin from '../../public/social/linkedin.svg'
 import mail from '../../public/social/mail.svg'
@@ -11,28 +12,36 @@ export default function Contact() {
 					Contact
 				</div>
 			</div>
-			<div className="contact_links">	
-					<Image
-						width={130}
-						height={130}
-						priority={true}
-						src={git}
-						className="contact_links_1 contact_links_link"
-					/>	
+			<div className="contact_links">
+					<Link href='https://github.com/OscarGomezL'>
+						<a target='_blank'>
+							<Image
+								width={130}
+								height={130}
+								priority={true}
+								src={git}
+								className="contact_links_1 contact_links_link"
+							/>	
+						</a>
+					</Link>
 					<Image
 						width={130}
 						height={130}
 						priority={true}
 						src={mail}
 						className="contact_links_1 contact_links_link"
-					/>	
-					<Image
-						width={130}
-						height={130}
-						priority={true}
-						src={linkedin}
-						className="contact_links_1 contact_links_link"
 					/>
+					<Link href='https://www.linkedin.com/in/oscargomezl/'>
+						<a target="_blank">
+							<Image
+								width={130}
+								height={130}
+								priority={true}
+								src={linkedin}
+								className="contact_links_1 contact_links_link"
+							/>
+						</a>
+					</Link>
 			</div>
 		</section>
 	)
