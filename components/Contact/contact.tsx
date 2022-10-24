1e6483d494e3aa0e7c48b8a0c412e5c0886a78f5
Oscar Gomez <oscarthedev@gmail.com>
2022-10-23 21:00:52 -0600

    [ADD] "contact.tsx" component structured completed & img added

--- components/Contact/contact.tsx
+++ components/Contact/contact.tsx
@@ -1,7 +1,39 @@
+import Image from 'next/future/image'
+import git from '../../public/social/git.svg'
+import linkedin from '../../public/social/linkedin.svg'
+import mail from '../../public/social/mail.svg'
+
 export default function Contact() {
 	return (
 		<section className="container contact">
-
+			<div className="contact_title_container">
+				<div className="contact_title">
+					Contact
+				</div>
+			</div>
+			<div className="contact_links">	
+					<Image
+						width={130}
+						height={130}
+						priority={true}
+						src={git}
+						className="contact_links_1 contact_links_link"
+					/>	
+					<Image
+						width={130}
+						height={130}
+						priority={true}
+						src={mail}
+						className="contact_links_1 contact_links_link"
+					/>	
+					<Image
+						width={130}
+						height={130}
+						priority={true}
+						src={linkedin}
+						className="contact_links_1 contact_links_link"
+					/>
+			</div>
 		</section>
 	)
 }
