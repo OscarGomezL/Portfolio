922743b90d00ea3f94eef1700c556f5454da6d86
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 12:17:45 -0600

    [ADD] "useClientSide" custom hook implemented in "title.tsx" of "About Me" section

--- components/AboutMe/title.tsx
+++ components/AboutMe/title.tsx
@@ -1,35 +1,17 @@
-import { useEffect } from "react";
 import { aboutMeTitleObserver } from '../../js/observer.js'
 import useHandleScroll from "../../hooks/useHandleScroll";
+import useClientSide from "../../hooks/useClientSide.jsx";
 
 export default function Title() {
 	const [handleScroll,sp]: [((event:Event)=>void),number] = useHandleScroll()
 	const styles = {
 		transform: `translateX(calc(-150% + ${sp * 6}%))`,
 	}
-
-	useEffect(() => {
-		async function waitForClientSide() {
-			return new Promise(resolve => {
-				if(typeof window !== "undefined") {
-					resolve(true)
-				}
-				else {
-					waitForClientSide()
-				}
-			});
-		}
-		async function asyncCall() {
-			const result = await waitForClientSide();
-			if(result) {
-				window.addEventListener('scroll', handleScroll)
-				const aboutMeTitle = document.querySelector('.aboutMe_title') as HTMLElement 
-				aboutMeTitleObserver().observe(aboutMeTitle)
-			}
-			else asyncCall()
-		}
-		asyncCall();
-	}, [])
+	useClientSide(()=>{
+		const aboutMeTitle = document.querySelector('.aboutMe_title') as HTMLElement 
+		aboutMeTitleObserver().observe(aboutMeTitle)
+		window.addEventListener('scroll', handleScroll)
+	})
 	return (
 		<div 
 			className="aboutMe_title"
