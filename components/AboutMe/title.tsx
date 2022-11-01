83b83a6dd8c0f9b226ea929ead6ca6914f12b4a6
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 12:49:00 -0600

    [ADD] "backgroundColor" function imported and bug fixed

--- components/AboutMe/title.tsx
+++ components/AboutMe/title.tsx
@@ -1,17 +1,19 @@
 import { aboutMeTitleObserver } from '../../js/observer.js'
+import backgroundColor from '../../js/backgroundColor.js'
 import useHandleScroll from "../../hooks/useHandleScroll";
-import useClientSide from "../../hooks/useClientSide.jsx";
+import useClientSide from "../../hooks/useClientSide";
 
 export default function Title() {
 	const [handleScroll,sp]: [((event:Event)=>void),number] = useHandleScroll()
-	const styles = {
-		transform: `translateX(calc(-150% + ${sp * 6}%))`,
-	}
 	useClientSide(()=>{
 		const aboutMeTitle = document.querySelector('.aboutMe_title') as HTMLElement 
 		aboutMeTitleObserver().observe(aboutMeTitle)
 		window.addEventListener('scroll', handleScroll)
 	})
+	const styles = {
+		transform: `translateX(calc(-150% + ${sp * 6}%))`,
+	}
+	backgroundColor(sp)
 	return (
 		<div 
 			className="aboutMe_title"
