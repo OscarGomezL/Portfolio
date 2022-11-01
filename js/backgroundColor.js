aefdb52d4d0b7b8d570223dc2456c93b327294d5
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 12:48:19 -0600

    [ADD] "backgroundColor.js" file for "backgroundColor" function

---
+++ js/backgroundColor.js
@@ -0,0 +1,6 @@
+export default function backgroundColor(sp) {
+	let bodyTag = document.querySelector('body');
+	if(sp <= 10) bodyTag.style.background = 'var(--n4)';
+	else if(sp <= 85) bodyTag.style.background = 'var(--n2)';
+	else if(sp <= 100) bodyTag.style.background = 'black';
+}
