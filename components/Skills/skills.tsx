11134bc0b59ff2772162dfbcbc7f48e14999de92
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 02:05:08 -0600

    [ADD] all components imported in "skills.tsx" section

--- components/Skills/skills.tsx
+++ components/Skills/skills.tsx
@@ -1,7 +1,13 @@
+import Description from "./description"
+import Title from "./title"
+import Images from "./images"
+
 export default function Skills() {
 	return (
 		<section className="container skills">
-
+			<Description/>
+			<Title/>
+			<Images/>
 		</section>
 	)
 }
