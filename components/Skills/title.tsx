24b9445f0b22e81826363596afa776ab9fa1f2e8
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 01:56:50 -0600

    [ADD] handleScroll implemented

--- components/Skills/title.tsx
+++ components/Skills/title.tsx
@@ -1,8 +1,32 @@
 import { useEffect } from "react";
+import useHandleScroll from "../../hooks/useHandleScroll";
 
 export default function Title() {
+	const [handleScroll,sp]: [((event:Event)=>void),number] = useHandleScroll()
+	const styles = {
+		transform: `translateX(calc(415% - ${sp * 6}%))`,
+	}
+
+	useEffect(() => {
+		async function waitForClientSide() {
+			return new Promise(resolve => {
+				if(typeof window !== "undefined") {
+					resolve(true)
+				}
+				else {
+					waitForClientSide()
+				}
+			});
+		}
+		async function asyncCall() {
+			const result = await waitForClientSide();
+			if(result) {window.addEventListener('scroll', handleScroll)}
+			else asyncCall()
+		}
+		asyncCall();
+	}, [])
 	return (
-		<div className="skills_title">
+		<div className="skills_title" style={styles}>
 			Skills
 		</div>
 	)
