3f5ae6696b7341b0c37dad7e6791902ff816afa3
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 01:53:26 -0600

    [ADD] custom hook "useHandleScroll" implemented in title.tsx component of "AboutMe.tsx" section

--- components/AboutMe/title.tsx
+++ components/AboutMe/title.tsx
@@ -1,21 +1,13 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { aboutMeTitleObserver } from '../../js/observer.js'
+import useHandleScroll from "../../hooks/useHandleScroll";
 
 export default function Title() {
-	const [sp, setSp] = useState(0)
+	const [handleScroll,sp]: [((event:Event)=>void),number] = useHandleScroll()
 	const styles = {
 		transform: `translateX(calc(-150% + ${sp * 6}%))`,
 	}
-	function handleScroll(event:{[key:string]:any}) { 
-		const { body, documentElement } = event.srcElement;
-		const scrollDistance:number = Math.max(body.scrollTop, documentElement.scrollTop);
-		const scrollPercentage:number = (scrollDistance / (documentElement.scrollHeight - documentElement.clientHeight) * 100);
-		setSp(scrollPercentage)
-		let bodyTag = document.querySelector('body') as HTMLElement;
-		if(scrollPercentage <= 10) bodyTag.style.background = 'var(--n4)';
-		else if(scrollPercentage <= 85) bodyTag.style.background = 'var(--n2)';
-		else if(scrollPercentage <= 100) bodyTag.style.background = 'black';
-	}
+
 	useEffect(() => {
 		async function waitForClientSide() {
 			return new Promise(resolve => {
