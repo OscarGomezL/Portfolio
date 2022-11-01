cc917afa9abc1f7df7f451944957f891c0290009
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 12:20:51 -0600

    [ADD] skills added to "description.tsx" component

--- components/Skills/description.tsx
+++ components/Skills/description.tsx
@@ -1,6 +1,12 @@
 export default function Description() {
 	return (
 		<div className="skills_description">
+			<div className="skills_description_1"> Frontend:</div>
+				Html, Css, Js, Bootstrap, Js, React, Next, Redux, Graphql <br/>
+			<div className="skills_description_1"> Backend:</div>
+				Mongo, Express, Node, Mongoose, Typescript <br/>
+			<div className="skills_description_1"> Complementary:</div>
+				Git, Github, Stripe, Photoshop, Figma <br/>
 		</div>
 	)
 }
