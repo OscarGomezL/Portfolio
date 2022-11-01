e178ff788069f7a10e58d403ce7c269b58b3913b
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 12:24:35 -0600

    [ADD] "useClientSide" custom hook to "title.tsx" component

--- components/Skills/title.tsx
+++ components/Skills/title.tsx
@@ -1,30 +1,13 @@
 import { useEffect } from "react";
 import useHandleScroll from "../../hooks/useHandleScroll";
+import useClientSide from "../../hooks/useClientSide";
 
 export default function Title() {
 	const [handleScroll,sp]: [((event:Event)=>void),number] = useHandleScroll()
 	const styles = {
 		transform: `translateX(calc(415% - ${sp * 6}%))`,
 	}
-
-	useEffect(() => {
-		async function waitForClientSide() {
-			return new Promise(resolve => {
-				if(typeof window !== "undefined") {
-					resolve(true)
-				}
-				else {
-					waitForClientSide()
-				}
-			});
-		}
-		async function asyncCall() {
-			const result = await waitForClientSide();
-			if(result) {window.addEventListener('scroll', handleScroll)}
-			else asyncCall()
-		}
-		asyncCall();
-	}, [])
+	useClientSide(()=> window.addEventListener('scroll', handleScroll))
 	return (
 		<div className="skills_title" style={styles}>
 			Skills
