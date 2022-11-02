64773f9a6bfba428b2323ede528e1e0317b4b61a
Oscar Gomez <oscarthedev@gmail.com>
2022-11-01 23:37:51 -0600

    [PATCH] bodyTag is now defined as a parameter not inside of the function for ssr purposes

--- js/backgroundColor.js
+++ js/backgroundColor.js
@@ -1,5 +1,4 @@
-export default function backgroundColor(sp) {
-	let bodyTag = document.querySelector('body');
+export default function backgroundColor(sp, bodyTag) {
 	if(sp <= 10) bodyTag.style.background = 'var(--n4)';
 	else if(sp <= 85) bodyTag.style.background = 'var(--n2)';
 	else if(sp <= 100) bodyTag.style.background = 'black';
