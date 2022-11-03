4b1cbb8ad9c2f8752e9b4373f1204c983710716d
Oscar Gomez <oscarthedev@gmail.com>
2022-11-03 16:15:22 -0600

    [ADD] intersectionObserver function called in "title.tsx" component

--- components/Skills/title.tsx
+++ components/Skills/title.tsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
 import useHandleScroll from "../../hooks/useHandleScroll";
 import useClientSide from "../../hooks/useClientSide";
+import { skillsTitleObserver} from "../../js/observer";
 
 export default function Title() {
 	const [handleScroll,sp]: [((event:Event)=>void),number] = useHandleScroll()
 	const styles = {
 		transform: `translateX(calc(415% - ${sp * 6}%))`,
 	}
-	useClientSide(()=> window.addEventListener('scroll', handleScroll))
+	useClientSide(()=> {
+		window.addEventListener('scroll', handleScroll)
+		const skillsTitle = document.querySelector('.skills_title') as HTMLElement 
+		skillsTitleObserver().observe(skillsTitle)
+	})
 	return (
 		<div className="skills_title" style={styles}>
 			Skills
