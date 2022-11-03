684f80603d42395de78bc1708dead86c45fb06d7
Oscar Gomez <oscarthedev@gmail.com>
2022-11-03 16:12:50 -0600

    [ADD] "skillsTitleObserver" to "observer.js" file

--- js/observer.js
+++ js/observer.js
@@ -11,3 +11,26 @@ export function aboutMeTitleObserver() {
 	) 
 	return observer 
 }
+export function skillsTitleObserver() {
+	const observer = new IntersectionObserver(
+		entries=>{
+			document.querySelectorAll('.skill_image').forEach(el=>{
+				if(entries[0].isIntersecting) {
+					el.classList.add('image_inited')
+				}
+			})
+			document.querySelectorAll('.skills_description_1').forEach(el=>{
+				if(entries[0].isIntersecting) {
+					el.classList.add('description_inited')
+				}
+			})
+			document.querySelectorAll('.skills_description_2').forEach(el=>{
+				if(entries[0].isIntersecting) {
+					el.classList.add('description_inited')
+					observer.unobserve(entries[0].target)
+				}
+			})
+		}
+	) 
+	return observer 
+}
