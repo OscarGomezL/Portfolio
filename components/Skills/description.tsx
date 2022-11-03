c50104498f6f57ffe4c5d2dbcdf16143c8f768c9
Oscar Gomez <oscarthedev@gmail.com>
2022-11-03 16:13:33 -0600

    [ADD] add another div to wrap descriptions in for animations

--- components/Skills/description.tsx
+++ components/Skills/description.tsx
@@ -2,11 +2,11 @@ export default function Description() {
 	return (
 		<div className="skills_description">
 			<div className="skills_description_1"> Frontend:</div>
-				Html, Css, Js, Bootstrap, Js, React, Next, Redux, Graphql <br/>
+				<div className="skills_description_2"> Html, Css, Js, Bootstrap, Js, React, Next, Redux, Graphql</div>
 			<div className="skills_description_1"> Backend:</div>
-				Mongo, Express, Node, Mongoose, Typescript <br/>
+				<div className="skills_description_2"> Mongo, Express, Node, Mongoose, Typescript</div>
 			<div className="skills_description_1"> Complementary:</div>
-				Git, Github, Stripe, Photoshop, Figma <br/>
+				<div className="skills_description_2"> Git, Github, Stripe, Photoshop, Figma</div><br/>
 		</div>
 	)
 }
