25214af65b5dae84308c1f3deb4e04398a5106bc
Oscar Gomez <oscarthedev@gmail.com>
2022-11-05 18:16:48 -0600

    [ADD] new next/font preload library implemented to avoid image shifting

--- components/Hero/name.tsx
+++ components/Hero/name.tsx
@@ -1,12 +1,14 @@
-import React, {useState, useEffect} from "react"
+import React from "react"
+import localFont from "@next/font/local"
 
+const Antonella = localFont({src:"../../assets/font/Antonella/Antonella.ttf"})
 export default function Name() {
 	return (
 		<React.Fragment>
-			<div className={`hero_name_name name_inited`}>
+			<div className={`hero_name_name name_inited ${Antonella.className}`}>
 				Óscar
 			</div>
-			<div className="hero_name_lastname lastname_inited">
+			<div className={`hero_name_lastname lastname_inited ${Antonella.className}`}>
 				Gómez
 			</div>
 		</React.Fragment>
