3e40c4a1365481b67ed846d09a1c3cf04461cd90
Oscar Gomez <oscarthedev@gmail.com>
2022-11-06 19:30:38 -0600

    [ADD] setProject  function added to "controls.tsx" component

--- components/Projects/controls.tsx
+++ components/Projects/controls.tsx
@@ -1,8 +1,28 @@
-export default function Controls() {
+interface skills_controls {
+	setProject:any
+}
+
+export default function Controls( { setProject } : skills_controls) {
 	return (
 		<div className="projects_controls">
-			<img src="/polygon_1.svg" alt="<"/>
-			<img src="/polygon_2.svg" alt=">"/>
+			<img 
+				src="/polygon_1.svg"
+				alt="<"
+				onClick={
+					()=>{
+						setProject((project:number)=> project > 1 ? project - 1 : 6)
+					}
+				}
+			/>
+			<img 
+				src="/polygon_2.svg" 
+				alt=">"
+				onClick={
+					()=>{
+						setProject((project:number)=> project < 6 ? project + 1 : 1)
+					}
+				}
+			/>
 		</div>
 	)
 }
