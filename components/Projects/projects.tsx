c62f91d620dd3f5bcd0d20c1e4a86501422fac01
Oscar Gomez <oscarthedev@gmail.com>
2022-11-06 19:31:32 -0600

    [ADD] typing to "getProjects" and passed info to components where needed

--- components/Projects/projects.tsx
+++ components/Projects/projects.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react"
+import getProjects from '../../data/projects'
 import Number from "./number"
 import Info from "./info"
 import Controls from "./controls"
 import Title from "./title"
 import Images from "./images"
 
+interface projects {
+	[key:number] : {
+		number:string,
+		info:{
+			title:string,
+			technologies: string[],
+			description: string
+		},
+		title:string
+	}
+}
+
 export default function Projects() {
+	const [project, setProject]:[number, any] = useState(1)
+	const projects:projects = getProjects()
 	return (
 		<section className="container projects">
 			<div className="projects_container_1">
-				<Number/>
-				<Info/>
-				<Title/>
-				<Controls/>
+				<Number project={ project } />
+				<Info project={ project } projects={projects } />
+				<Title project={ project } projects={ projects } />
+				<Controls setProject={ setProject } />
 			</div>
 			<div className="projects_container_2">
-				<Images/>
+				<Images project={project} />
 			</div>
 		</section>
 	)
