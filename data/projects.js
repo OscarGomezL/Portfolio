0466bb2995ce7b63c800202e1b397fe661daafd4
Oscar Gomez <oscarthedev@gmail.com>
2022-11-06 20:12:33 -0600

    [PATCH] some typos

--- data/projects.js
+++ data/projects.js
@@ -4,7 +4,7 @@ export default function getProjects() {
 			1:{
 				number:"01",
 				info: {
-					title: "Snickers",
+					title: "Snickers Arcade",
 					technologies: ["Html","Bootstrap","Css"],
 					description: "Snickers Arcade is a project in which I participated while working in Accenture. Snickers made a campaign in my country for which I worked along with my team developing the frontend of the software they used for the giveaway promotion.",
 				},
@@ -13,9 +13,9 @@ export default function getProjects() {
 			2:{
 				number:"02",
 				info: {
-					title: "GameCommerce API",
+					title: "GameCommerce",
 					technologies: ["Node","Express","Mongoose","Mongo"],
-					description: "GameCommerce, from the backend perspective, is an API based on n-layer arquitecture with: Stripe Implementation, JWT token authentification, CRUD and all the necessities of an ecommerce backend"
+					description: "GameCommerce, from the backend perspective, is an API with a n-layer arquitecture. It includes: Stripe Implementation, JWT token authentification, CRUD functionalities, and all the needs of an ecommerce backend."
 				},
 				title:"Backend"
 			},
@@ -24,16 +24,16 @@ export default function getProjects() {
 				info: {
 					title: "GameCommerce",
 					technologies: ["Next","Redux", "Typescript"],
-					description: "GameCommerce, from the frontend perspective, is a software I made to give use to the backend I created, it makes use of all the functionalities including log ups, log ins, authentication processes, stripe interactions, data and images fetching, and more",
+					description: "GameCommerce, from the frontend perspective, is a software I made to give use to the backend I created, it makes use of all the functionalities including log ups, log ins, authentication processes, stripe interactions, data and images fetching, and more.",
 				},
 				title:"Frontend"
 			},
 			4:{
 				number:"04",
 				info: {
-					title: "BetterAmbience",
+					title: "Better Ambience",
 					technologies: ["Next","Use-Sound"],
-					description: "BetterAmbience was my first project ever, betterAmbience is a software designed to improve your experiencie with relaxating sounds of the users preference. The user could combine sounds and adjust the volume at will.",
+					description: "BetterAmbience was my first project, betterAmbience is a software designed to improve your experience with relaxating sounds of the your preference. The user can combine sounds and adjust the volume of each sound at will.",
 				},
 				title:"Frontend"
 			},
@@ -41,17 +41,17 @@ export default function getProjects() {
 				number:"05",
 				info: {
 					title: "DaysBetween",
-					technologies: ["Next","Use-Sound"],
-					description: "DaysBetween is a simple software which ease the process of calculating the time between two given dates.The design is simple and the concept is aswell, but it was a good introduction to nextjs" ,
+					technologies: ["Next"],
+					description: "DaysBetween is a simple software which eases the process of calculating the time between two given dates.The design is simple and the concept is aswell, but it was a good introduction to nextjs." ,
 				},
 				title:"Frontend"
 			},
 			6:{
 				number:"06",
 				info: {
-					title: "AnotherPomodoroApp",
+					title: "AnotherPomodoro",
 					technologies: ["Next","Redux"],
-					description: "AnotherPomodoroApp is a web pomodoro timer software, a pretty simple application which I made to practice browser-side API's and global state using redux with nextjs",
+					description: "AnotherPomodoroApp is a web pomodoro timer software, a pretty simple application which I made to practice browser-side API's and global state using server-side rendering and redux with nextjs.",
 				},
 				title:"Frontend"
 			},
