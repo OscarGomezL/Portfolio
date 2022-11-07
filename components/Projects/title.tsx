a71ccad40444c3822540934996ca22f65008a298
Oscar Gomez <oscarthedev@gmail.com>
2022-11-06 19:46:11 -0600

    [ADD] data implemented according to state in "title.tsx" component

--- components/Projects/title.tsx
+++ components/Projects/title.tsx
@@ -1,7 +1,22 @@
-export default function Title() {
+interface skills_title {
+	project:number,
+	projects: {
+		[key:number] : {
+			number:string,
+			info:{
+				title:string,
+				technologies: string[],
+				description: string
+			},
+			title:string
+		}
+	}
+}
+
+export default function Title( {project, projects} : skills_title ) {
 	return (
 		<div className="projects_title">
-			Frontend
+			{projects[project].title}
 		</div>
 	)
 }
