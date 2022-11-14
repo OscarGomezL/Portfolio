9b1877b6d4287d9196677932493b57e2beb955a7
Oscar Gomez <oscarthedev@gmail.com>
2022-11-13 20:40:56 -0600

    [ADD] imported different images & added them to the section

--- components/Projects/images.tsx
+++ components/Projects/images.tsx
@@ -1,7 +1,29 @@
 import Image from 'next/image'
-import ipad from '../../public/projects/snickers/snickers_ipad.png'
-import ipad_2 from '../../public/projects/snickers/snickers_ipad2.png'
-import phone from '../../public/projects/snickers/snickers_phone.png'
+//snickers
+import ipad_snickers from '../../public/projects/snickers/snickers_ipad.png'
+import ipad_2_snickers from '../../public/projects/snickers/snickers_ipad2.png'
+import phone_snickers from '../../public/projects/snickers/snickers_phone.png'
+//gameCommerceBackend
+import ipad_gameCommerce_backend from '../../public/projects/gameCommerceBackend/snickers_ipad.png'
+import ipad_2_gameCommerce_backend from '../../public/projects/gameCommerceBackend/snickers_ipad2.png'
+import phone_gameCommerce_backend from '../../public/projects/gameCommerceBackend/snickers_phone.png'
+//GameCommerceFrontend
+import ipad_gameCommerce_frontend from '../../public/projects/gameCommerceFrontend/snickers_ipad.png'
+import ipad_2_gameCommerce_frontend from '../../public/projects/gameCommerceFrontend/snickers_ipad2.png'
+import phone_gameCommerce_frontend from '../../public/projects/gameCommerceFrontend/snickers_phone.png'
+//betterAmbience
+import ipad_betterAmbience from '../../public/projects/betterAmbience/snickers_ipad.png'
+import ipad_2_betterAmbience from '../../public/projects/betterAmbience/snickers_ipad2.png'
+import phone_betterAmbience from '../../public/projects/betterAmbience/snickers_phone.png'
+//daysBetween
+import ipad_daysBetween from '../../public/projects/daysBetween/snickers_ipad.png'
+import ipad_2_daysBetween from '../../public/projects/daysBetween/snickers_ipad2.png'
+import phone_daysBetween from '../../public/projects/daysBetween/snickers_phone.png'
+//anotherPomodoro
+import ipad_anotherPomodoro from '../../public/projects/anotherPomodoroApp/snickers_ipad.png'
+import ipad_2_anotherPomodoro from '../../public/projects/anotherPomodoroApp/snickers_ipad2.png'
+import phone_anotherPomodoro from '../../public/projects/anotherPomodoroApp/snickers_phone.png'
+
 
 interface skills_images {
 	project : number,
@@ -16,16 +38,96 @@ export default function Images( {project} : skills_images ) {
 						width={286}
 						height={440}
 						priority={true}
-						src={ipad}
-						className="img_projects_1"
+						src={ipad_snickers}
+						className={`img_projects_1  ${project == 1 ? "img_projects_inited" : ""}`}
 					/>
 					<Image
 						alt='snickers_image'
 						width={158}
 						height={321}
 						priority={true}
-						src={phone}
-						className="img_projects_2"
+						src={phone_snickers}
+						className={`img_projects_2  ${project == 1 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={286}
+						height={440}
+						priority={true}
+						src={ipad_gameCommerce_backend}
+						className={`img_projects_1  ${project == 2 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={158}
+						height={321}
+						priority={true}
+						src={phone_gameCommerce_backend}
+						className={`img_projects_2  ${project == 2 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={286}
+						height={440}
+						priority={true}
+						src={ipad_gameCommerce_frontend}
+						className={`img_projects_1  ${project == 3 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={158}
+						height={321}
+						priority={true}
+						src={phone_gameCommerce_frontend}
+						className={`img_projects_2  ${project == 3 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={286}
+						height={440}
+						priority={true}
+						src={ipad_betterAmbience}
+						className={`img_projects_1  ${project == 4 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={158}
+						height={321}
+						priority={true}
+						src={phone_betterAmbience}
+						className={`img_projects_2  ${project == 4 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={286}
+						height={440}
+						priority={true}
+						src={ipad_daysBetween}
+						className={`img_projects_1  ${project == 5 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={158}
+						height={321}
+						priority={true}
+						src={phone_daysBetween}
+						className={`img_projects_2  ${project == 5 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={286}
+						height={440}
+						priority={true}
+						src={ipad_anotherPomodoro}
+						className={`img_projects_1  ${project == 6 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={158}
+						height={321}
+						priority={true}
+						src={phone_anotherPomodoro}
+						className={`img_projects_2  ${project == 6 ? "img_projects_inited" : ""}`}
 					/>
 			</div>
 			<div className='projects_images_2'>
@@ -34,8 +136,48 @@ export default function Images( {project} : skills_images ) {
 						width={440}
 						height={286}
 						priority={true}
-						src={ipad_2}
-						className="img_projects_3"
+						src={ipad_2_snickers}
+						className={`img_projects_3  ${project == 1 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={440}
+						height={286}
+						priority={true}
+						src={ipad_2_gameCommerce_backend}
+						className={`img_projects_3  ${project == 2 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={440}
+						height={286}
+						priority={true}
+						src={ipad_2_gameCommerce_frontend}
+						className={`img_projects_3  ${project == 3 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={440}
+						height={286}
+						priority={true}
+						src={ipad_2_betterAmbience}
+						className={`img_projects_3  ${project == 4 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={440}
+						height={286}
+						priority={true}
+						src={ipad_2_daysBetween}
+						className={`img_projects_3  ${project == 5 ? "img_projects_inited" : ""}`}
+					/>
+					<Image
+						alt='snickers_image'
+						width={440}
+						height={286}
+						priority={true}
+						src={ipad_2_anotherPomodoro}
+						className={`img_projects_3  ${project == 6 ? "img_projects_inited" : ""}`}
 					/>
 			</div>
 		</div>
