c5d015e9c1eaf5d055b1e65c6ee4e08c370b93a2
Oscar Gomez <oscarthedev@gmail.com>
2023-01-08 10:58:16 -0600

    [ADD] description to text in "aboutMe" section

--- components/AboutMe/text.tsx
+++ components/AboutMe/text.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Text() {
 	return (
 		<div className='aboutMe_text_container'>
-			<div className='aboutMe_line_1 aboutMe_line'>An "About Me"</div>
-			<div className='aboutMe_line_2 aboutMe_line'>Section is here</div>
-			<div className='aboutMe_line_3 aboutMe_line'>A description Will</div>
-			<div className='aboutMe_line_4 aboutMe_line'>Be Right Here</div>
+			<div className='aboutMe_line_1 aboutMe_line'>I'm a software engineer</div>
+			<div className='aboutMe_line_2 aboutMe_line'>with 1 year of working experience</div>
+			<div className='aboutMe_line_3 aboutMe_line'>with a serious passion about</div>
+			<div className='aboutMe_line_4 aboutMe_line'>frontend development and UX</div>
 		</div>
 	)
 }
