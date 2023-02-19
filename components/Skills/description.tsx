503c979a44fd066f2bbe92a75d80a68b7693703c
Oscar Gomez <oscarthedev@gmail.com>
2023-02-18 22:57:48 -0600

    [PATCH] fixed typo

--- components/Skills/description.tsx
+++ components/Skills/description.tsx
@@ -2,7 +2,7 @@ export default function Description() {
 	return (
 		<div className="skills_description">
 			<div className="skills_description_1"> Frontend:</div>
-				<div className="skills_description_2"> Html, Css, Js, Bootstrap, Js, React, Next, Redux, Graphql</div>
+				<div className="skills_description_2"> Html, Css, Js, Bootstrap, React, Next, Redux, Graphql</div>
 			<div className="skills_description_1"> Backend:</div>
 				<div className="skills_description_2"> Mongo, Express, Node, Mongoose, Typescript</div>
 			<div className="skills_description_1"> Complementary:</div>
